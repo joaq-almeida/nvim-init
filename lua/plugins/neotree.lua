1eb7c69ad91cef7a62fe51a456681cc3068fa9fe
joaq-almeida <joaquimrafael30@hotmail.com>
2023-07-03 12:39:04 -0300

    show hidden files

---
+++ lua/plugins/neotree.lua
@@ -0,0 +1,12 @@
+return {
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    opt = {
+      filesystem = { 
+        filtered_items = { 
+          hide_dotfiles = false
+        }
+      }
+    }
+  }
+}
